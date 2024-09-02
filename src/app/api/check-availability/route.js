a52deaac910d95f0707b2394909597fbae4a2f06
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2024-09-02 13:49:33 -0300

    Conexion a DB host.

--- src/app/api/check-availability/route.js
+++ src/app/api/check-availability/route.js
@@ -1,10 +1,60 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// export async function POST(request) {
+//   const { office, startTime, endTime, appointmentId } = await request.json();
+
+//   try {
+//     const start = new Date(startTime);
+//     const end = new Date(endTime);
+
+//     // Verifica si las fechas son válidas
+//     if (isNaN(start.getTime()) || isNaN(end.getTime())) {
+//       throw new Error('Invalid date or time value.');
+//     }
+//     console.log(appointmentId);
+//     // Encuentra citas que se solapan con el rango solicitado
+
+//     const existingAppointments = await prisma.appointment.findMany({
+//       where: {
+//         office: parseInt(office),
+//         startTime: {
+//           lt: end,
+//         },
+//         endTime: {
+//           gt: start,
+//         },
+//         // Excluye el turno que se está editando
+        
+//         NOT: {
+//           id: parseInt(appointmentId), // Asegúrate de que appointmentId sea un número entero
+//         },
+//       },
+//     });
+
+//     const isAvailable = existingAppointments.length === 0;
+
+//     return new Response(
+//       JSON.stringify({ available: isAvailable }),
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error('Error checking availability:', error);
+//     return new Response(
+//       JSON.stringify({ error: 'Error checking availability' }),
+//       { status: 500 }
+//     );
+//   }
+// }
+
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export async function POST(request) {
   const { office, startTime, endTime, appointmentId } = await request.json();
-
+  console.log(appointmentId);
   try {
     const start = new Date(startTime);
     const end = new Date(endTime);
@@ -14,7 +64,7 @@ export async function POST(request) {
       throw new Error('Invalid date or time value.');
     }
 
-    // Encuentra citas que se solapan con el rango solicitado
+    // Si appointmentId no está definido, omite el filtro NOT
     const existingAppointments = await prisma.appointment.findMany({
       where: {
         office: parseInt(office),
@@ -24,10 +74,12 @@ export async function POST(request) {
         endTime: {
           gt: start,
         },
-        // Excluye el turno que se está editando
-        NOT: {
-          id: parseInt(appointmentId), // Asegúrate de que appointmentId sea un número entero
-        },
+        // Si appointmentId está definido, exclúyelo de la consulta
+        ...(appointmentId && {
+          NOT: {
+            id: parseInt(appointmentId),
+          },
+        }),
       },
     });
 
