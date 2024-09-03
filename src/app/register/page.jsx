c4967c5c0040704b9a0ecf18e20d288905d61199
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2024-09-03 14:11:14 -0300

    Register colors

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -33,7 +33,7 @@ export default function Register() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className=" flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-semibold mb-4">Registro</h1>
         {error && <p className="text-red-500 mb-4">{error}</p>}
@@ -45,7 +45,7 @@ export default function Register() {
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:text-sm"
               required
             />
           </div>
@@ -56,7 +56,7 @@ export default function Register() {
               type="text"
               value={surname}
               onChange={(e) => setSurname(e.target.value)}
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:text-sm"
               required
             />
           </div>
@@ -67,7 +67,7 @@ export default function Register() {
               type="text"
               value={dni}
               onChange={(e) => setDni(e.target.value)}
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:text-sm"
               required
             />
           </div>          
@@ -78,7 +78,7 @@ export default function Register() {
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:text-sm"
               required
             />
           </div>
@@ -89,19 +89,19 @@ export default function Register() {
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-slate-500 focus:ring-slate-500 sm:text-sm"
               required
             />
           </div>          
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full bg-slate-600 text-white py-2 px-4 rounded-md hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             Registrar
           </button>
         </form>
         <p className="mt-4 text-center text-sm">
-          ¿Ya tienes una cuenta? <a href="/login" className="text-indigo-600 hover:text-indigo-700">Inicia sesión</a>
+          ¿Ya tienes una cuenta? <a href="/login" className=" font-bold text-slate-600 hover:text-slate-700">Inicia sesión</a>
         </p>
       </div>
     </div>
